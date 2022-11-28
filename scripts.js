426627f6ac783ad8e349b5033dc2e7ec22850233
tinbob655 tinbob655 <wrutlan07@gmail.com>
2022-11-28 21:24:36 +0000

    Mobile links now work correctly

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function detectmobile() {
-    const isMobile = navigator.userAgentData.mobile;
-    var path = window.location.pathname;
-    console.log(path);
-    var page = path.split("/").pop();
-    console.log( page );
-}
-
-window.onload = detectmobile();

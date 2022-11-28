03988a101eb6c7b34b0417920c992d37a50151c1
tinbob655 tinbob655 <wrutlan07@gmail.com>
2022-11-28 20:55:05 +0000

    Mobile integration

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 function detectmobile() {
     const isMobile = navigator.userAgentData.mobile;
     var url = window.location.pathname;
-    var filename = url.toLowerCase.substring(url.lastIndexOf('/')+1);
+    var filename = url.substring(url.lastIndexOf('/')+1);
+    console.log("mobile-"+filename);
 }
 
 window.onload = detectmobile();

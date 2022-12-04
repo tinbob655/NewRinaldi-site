e8b2029b99af6dfa98c3009fb7ea7a329595435c
tinbob655 tinbob655 <wrutland07@gmail.com>
2022-12-04 14:26:36 +0000

    Mobile menu is now sick

--- scripts.js
+++ scripts.js
@@ -1,15 +1,7 @@
-let toggle = 0;
-function toggle_dropdown_menu() {
-    if (toggle %2 == 0) {
-        document.getElementById('menu-dropdown').classList.add('show-dropdown');
-        document.getElementById('menu-dropdown').classList.remove('hide-dropdown');
-        toggle++;
-    }
-    else if (toggle %2 == 1) {
-        document.getElementById('menu-dropdown').classList.add('hide-dropdown');
-        document.getElementById('menu-dropdown').classList.remove('show-dropdown');
-        toggle++;
-    }
+function openNav() {
+    document.getElementById("sidenav").style.width = "25%";
 }
 
-window.onload = (toggle = 0);
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0%";
+}

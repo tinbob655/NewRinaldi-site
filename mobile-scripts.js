bd2c9483bbad315b41b6e78ccf2fae35c781988a
tinbob655 tinbob655 <wrutland07@gmail.com>
2023-01-01 14:44:45 +0000

    Optimised mobile detection

---
+++ mobile-scripts.js
@@ -0,0 +1,19 @@
+function init() {
+    if (navigator.userAgentData.mobile == false) {
+        var HTMLfilename = document.location.href;
+        var filename = HTMLfilename.substring(HTMLfilename.lastIndexOf('/') + 1);
+        filename = filename.replace('mobile-', '');
+        //document.location = filename;
+        console.log(filename)
+    };
+};
+
+function openNav() {
+    document.getElementById("sidenav").style.width = "33%";
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0%";
+}
+
+window.onload = init()
--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
-function openNav() {
-    document.getElementById("sidenav").style.width = "33%";
-}
+function init() {
+    if (navigator.userAgentData.mobile == true) {
+        var HTMLfilename = document.location.href;
+        var filename = HTMLfilename.substring(HTMLfilename.lastIndexOf('/') + 1);
+        filename = 'mobile-'+filename;
+        document.location = filename;
+        console.log(filename)
+    };
+};
 
-function closeNav() {
-    document.getElementById("sidenav").style.width = "0%";
-}
+window.onload = init();

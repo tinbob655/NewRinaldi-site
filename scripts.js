bc7d14a1df9ab3bea00ec03c68ac83b6f33450cb
tinbob655 tinbob655 <wrutland07@gmail.com>
2023-01-08 14:40:49 +0000

    Added much better audio and transition to desktop

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 function init() {
-    if (navigator.userAgentData.mobile == true) {
+    if (navigator.userAgentData.mobile == true) {      //mobile detection and redirection
         var HTMLfilename = document.location.href;
         var filename = HTMLfilename.substring(HTMLfilename.lastIndexOf('/') + 1);
         if (filename != '') {
@@ -11,11 +11,24 @@ function init() {
         document.location = filename;
     };
     const location = document.location.href.substring(document.location.href.lastIndexOf('/') +1);
-    if (location != 'index.html' && location != 'music.html') {
+    if (location == 'index.html') {         //introbox activation checker
+        if (sessionStorage.getItem('first load') == 'false') {
+            setTimeout(() => {
+                document.getElementById('introbox').classList.add('cleared');
+                setTimeout(() => {
+                    document.getElementById('introbox').style.with = 0;
+                    document.getElementById('introbox').style.height = 0;
+                }, 1001);
+            }, 1000);
+        };
+    };
+    if ((location != 'index.html' || sessionStorage.getItem('first load') == 'false') && location != 'music.html') {  //audio activation
         setTimeout(()=> {
-            next_track();
+            first_track('not initial');
+            audio_fadin();
         }, 1000);
     };
+    sessionStorage.setItem('first load', 'false');
 };
 
 const gallery_images = ['images/Merch/black hoodie.webp',
@@ -74,18 +87,55 @@ function gallery_move() {
 };
 
 function next_track() {
+    console.log('next track fired');
     const audio = document.getElementById('audio');
-    audio.pause()
-    if (track%2 == 0) {
-        audio.src = 'Music/R4R theme.mp3';
+    audio.pause();
+    audio.volume = 1.0;
+    console.log(sessionStorage.getItem('current track'));
+    if (clense_src_input(sessionStorage.getItem('current track')) == 'Jumping Around.mp3') {
+        audio.src = 'Music/Skidaddle.mp3';
     }
     else {
-        audio.src = 'Music/March Of Leader Ransome.mp3';
+        audio.src = 'Music/Jumping Around.mp3'   
     };
-    track++;
-    setTimeout(() => {
+    sessionStorage.setItem('current track', audio.src);
+    audio.currentTime = 0;
+    audio.play();
+};
+
+function audio_fadin() {
+    const audio = document.getElementById('audio');
+    async function fadin() {
+        const timer = milliseconds => new Promise (res => setTimeout(res, milliseconds));
+        for (let i = 0; 0 < 100; i++) {
+            audio.volume = i/100;
+            await timer(50);
+        };
+    };
+    fadin();
+};
+
+function first_track(initial) {
+    console.log('first track fired. initial = '+initial);
+    const audio = document.getElementById('audio');
+    audio.load();
+    if (initial == 'initial') {
+        var track = Math.floor(Math.random() *2);
+        console.log(track);
+        if (track == 0) {
+            audio.src = 'Music/Jumping Around.mp3';
+        }
+        else {
+            audio.src = 'Music/Skidaddle.mp3';
+        }; 
+        audio.play();     
+    } 
+    else if (initial == 'not initial') {
+        audio.src = sessionStorage.getItem('current track');
+        audio.currentTime = sessionStorage.getItem('audio duration');
         audio.play();
-    }, 500);
+    };
+    sessionStorage.setItem('current track', audio.src);
 };
 
 function introbox_cleared() {
@@ -95,17 +145,27 @@ function introbox_cleared() {
         introbox.style.with = 0;
         introbox.style.height = 0;
     }, 1001);
-    next_track();
+    first_track('initial');
 };
 
 function change_page(page) {
+    const audio = document.getElementById('audio');
     document.body.style.opacity = 0.0;
-    setTimeout(() => {
+    async function mute_audio() {
+        const timer = miliseconds => new Promise (res => setTimeout(res, miliseconds));
+        for (let a = 0; a < 10; a++) {
+            audio.volume = 1 - (a/10);
+            await timer(100);
+        };
+        await timer(100);
+        audio.volume = 0.0;
+        sessionStorage.setItem('audio duration', audio.currentTime);
+        audio.pause();
         document.location = page;
-    }, 501);
+    };
+    mute_audio();
 };
 
 gallery_num = 1;
-var track = Math.floor(Math.random() *2);
 
 window.onload = init();
